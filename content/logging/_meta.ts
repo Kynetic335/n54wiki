285c18c77357ae9ba1e5841822f5b3b8ce116028
Kynetic335 <jmohoska88@gmail.com>
2026-06-09 12:16:22 -0700

    Upgrade N54 diagnostics and logging education pages

---
+++ content/logging/_meta.ts
@@ -0,0 +1,24 @@
+const meta = {
+  "index": "Logging",
+  "mhd-channels": "MHD Channels",
+  "boost-target-vs-actual": "Boost Target vs Actual",
+  "wgdc": "Logging WGDC",
+  "throttle-angle": "Logging Throttle Angle",
+  "load-requested-vs-actual": "Load Requested vs Actual",
+  "timing-corrections": "Logging Timing Corrections",
+  "lambda-afr": "Logging Lambda / AFR",
+  "stft-ltft": "Logging STFT / LTFT",
+  "hpfp-pressure": "Logging HPFP Pressure",
+  "lpfp-pressure": "Logging LPFP Pressure",
+  "iat": "Logging IAT",
+  "coolant-temp": "Logging Coolant Temp",
+  "oil-temp": "Logging Oil Temp",
+  "gear-rpm": "Logging Gear & RPM",
+  "reading-a-3rd-gear-pull": "Reading a 3rd Gear Pull",
+  "unsafe-logs": "Unsafe Logs",
+  "fuel-limits": "Logging Fuel Limits",
+  "boost-control-problems": "Logging Boost Control Problems",
+  "timing-problems": "Logging Timing Problems",
+}
+
+export default meta
